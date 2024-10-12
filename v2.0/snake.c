1a62e1f4abe547908dea010700d3c06ea2aa9338
Rpid <2059162753@qq.com>
2024-10-12 13:35:45 +0800

    feat：新增了终端的运行适配代码(add new terminal adaptation code)

--- v2.0/snake.c
+++ v2.0/snake.c
@@ -2,11 +2,27 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdbool.h> 
 
-static snakeBody *tail;
-static int map_horizontal;
-static int map_vertical;
+/**
+ * @brief 对 Window 和 Linux 库的 选择适配
+*/
+#ifdef _WIN32
+    #include <conio.h>      // 包含 _kbhit() 和 _getch()
+    #include <windows.h>    // 包含 Sleep() 函数
+#else
+    #include <termios.h>    // linux 终端设置
+    #include <unistd.h>     // linux 内核系统调用
+    #include <fcntl.h>      // linux 文件描述符 操作接口
+    // linux 终端原始状态存储变量
+    typedef struct termios terminal;      // 终端状态结构体 改名
+    static terminal terminal_orig_status;  // 保存原始终端设置
+#endif
+
+static snakeBody *tail = NULL;
+static int map_horizontal = 0;
+static int map_vertical = 0;
 
 /**
  * @author      Exrpid
@@ -61,14 +77,14 @@ void MapSizeInit(int lenght_x, int width_y){
 char **MapSpaceCreate(){
     char **map_box = (char**)malloc(map_horizontal * sizeof(char*));
     if(!map_box){
-        printf("地图空间分配失败");
+        printf("地图空间分配失败\n");
         return NULL;
     }
     for(int i = 0; i < map_horizontal; i++)
         // 多出来的两格 装 '\n' (map_vertical) '\0' (map_vertical + 1)
         map_box[i] = (char*)malloc((map_vertical + 2) * sizeof(char));
     if(!map_box){
-        printf("地图空间分配失败");
+        printf("地图空间分配失败\n");
         return NULL;
     }
     return map_box;
@@ -101,7 +117,6 @@ void MapInit(char **map){
             map[i][j] = 'W';
         }
     }
-
 }
 
 /**
@@ -112,13 +127,13 @@ void MapInit(char **map){
  * @param       food 食物
  * @param       place 位置
 */
-void SnakeInit(snakeHead *head, snakeBody *body, snakeFood *food, char **map, char *front_buffer, int place){
+void SnakeInit(snakeHead *head, snakeBody *body, snakeFood *food, char **map, char *front_buffer){
     // place 为四位整数 前两位为 x 后两位为 y
-    head->x = (place / 100) / 2;
-    head->y = (place % 100) / 2;
+    head->x = map_horizontal / 2;
+    head->y = map_vertical  / 2;
     head->direction = RIGHT;
     map[head->y][head->x] = 'H';
-    printf("----- 蛇头初始化 OK -----");
+    printf("----- 蛇头初始化 OK -----\n");
     body->x = head->x - 1;
     body->y = head->y;
     body->direction = head->direction;
@@ -130,15 +145,15 @@ void SnakeInit(snakeHead *head, snakeBody *body, snakeFood *food, char **map, ch
         tail = tail->next;
         map[tail->y][tail->x] = 'B';
     }
-    printf("----- 蛇身初始化 OK -----");
-    food->x = rand() % (place / 100) + 1;
-    food->y = rand() % (place / 100) + 1;
+    printf("----- 蛇身初始化 OK -----\n");
+    food->x = rand() % map_horizontal + 1;
+    food->y = rand() % map_horizontal + 1;
     food->flag = true;
     map[food->y][food->x] = 'F';
-    printf("----- 食物初始化 OK -----");
+    printf("----- 食物初始化 OK -----\n");
     for(int i = 0; i < map_vertical; i++)
         strcat(front_buffer,map[i]);
-    printf("----- 初始帧绘制 OK -----");
+    printf("----- 初始帧绘制 OK -----\n");
 }
 
 /**
@@ -147,7 +162,7 @@ void SnakeInit(snakeHead *head, snakeBody *body, snakeFood *food, char **map, ch
  * @param       node 尾巴节点
  * @return      新生成的尾部节点
 */
-snakeBody *Snake_add_new_node(snakeBody *node){
+snakeBody *SnakeAddNewNode(snakeBody *node){
     snakeBody *body = (snakeBody*)malloc(sizeof(snakeBody));
     switch(node->direction){
         case UP: body->x = node->x; body->y = node->y + 1; break;
@@ -161,4 +176,39 @@ snakeBody *Snake_add_new_node(snakeBody *node){
     return body;
 }
 
+/**
+ * @author      Exrpid
+ * @brief       终端模式和设置初始化
+*/
+void TerminalInit(){
+#ifdef _WIN32
+    system("cls");
+    printf("Windows Terminal Init OK\n");
+#else
+    // 新终端状态存储变量
+    terminal terminal_new_status;
+    // 存储原始终端状态 以便于 “ 复位 ”
+    tcgetattr(0, &terminal_orig_status);
+    tcgetattr(0, &terminal_new_status);
+    terminal_new_status.c_lflag &= ~ICANON; // 取消终端 “ 规范 ” 模式
+    terminal_new_status.c_lflag &= ~ECHO;   // 关闭终端 “ 回显 ” 模式
+    tcsetattr(0, TCSANOW, &terminal_new_status); //使能 终端新状态
+    system("clear");
+    printf("Linux Terminal Init OK\n");
+#endif
+}
 
+/**
+ * @author      Exrpid
+ * @brief       终端状态复位
+*/
+void TerminalBack(){
+#ifdef _WIN32
+    system("cls");
+    printf("Windows Terminal Status Back OK\n");
+#else
+    tcsetattr(0, TCSANOW, &terminal_orig_status);
+    system("clear");
+    printf("Linux Terminal Status Back OK\n");
+#endif
+}
--- v2.0/snake.h
+++ v2.0/snake.h
@@ -16,11 +16,22 @@ typedef struct SnakeFood snakeFood;
 // extern snakeBody *tail_flag;
 
 /* 函数声明 */
+// 地图大小初始化
 void MapSizeInit(int lenght_x, int width_y);
+// 地图控制分配初始化
 char **MapSpaceCreate();
+// 地图缓冲区空间分配
 char *MapBufferSpaceCreate();
+// 地图初始化 填充 空地和墙壁
 void MapInit(char **map);
-void SnakeInit(snakeHead *head, snakeBody *body, snakeFood *food, char **map, char *front_buffer, int place);
-snakeBody *Snake_add_new_node(snakeBody *node);
+// “ 蛇 ” 初始化 蛇头 蛇身 初始画面帧
+void SnakeInit(snakeHead *head, snakeBody *body, snakeFood *food, char **map, char *front_buffer);
+// 新增蛇节点
+snakeBody *SnakeAddNewNode(snakeBody *node);
+// 终端状态初始化
+void TerminalInit();
+// 终端状态复位
+void TerminalBack();
+
 
 #endif
